5f9b7713c0985837f8859379531e514749b147bc
John Moore <jmoore15@bu.edu>
2015-04-02 22:02:00 -0400

    server update

--- server/app.py
+++ server/app.py
@@ -14,9 +14,11 @@ def list_apks_for_device():
     if 'deviceid' not in request.args:
     	raise InvalidRequestError()
     print "Received device ID: " + request.args['deviceid']
-    all_apks = ['obscure.apk', 'censor.apk']
+    all_apks = [{'apkname':'obscure.apk', 'servicetostart':'edu.bu.ec700.john.obscuremodule/edu.bu.ec700.john.obscuremodule.OverlayService', 'config':[{'action':'edu.bu.ec700.john.action.obscuremodule', 'resend':true, 'extras':{'type': 1, 'filterstring':'newfilterstr'}}]}, {'apkname':'censor.apk', 'servicetostart':'edu.bu.ec700.john.censormodule/edu.bu.ec700.john.censormodule.CensorService'}]
     return make_response(jsonify({'apks': all_apks}), 200)
 
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=1337, threaded=True)
+
+% TODO: Add database based on device ID
